16d1860ff029164f9c2ca786215a401b5c590414
Roman Steiner <rasteiner@users.noreply.github.com>
2025-05-12 11:54:31 +0200

    Add files via upload

--- helpers.php
+++ helpers.php
@@ -5,11 +5,41 @@ if (function_exists('icon')) {
 }
 
 use rasteiner\iconify\Icon as Icon;
+use Kirby\Cms\File;
 
-function icon(string $id, ...$attrs): string {
-    $icon = Icon::new($id);
-    if ($icon) {
-        return $icon->use(...$attrs);
+function icon(string|File $id, ...$attrs): string {
+    if($id instanceof File) {
+        if ($id->asset()->extension() !== 'svg') {
+            return '';
+        }
+
+        // parse xml document
+        $xml = simplexml_load_string($id->read());
+        if ($xml === false) {
+            return '';
+        }
+
+        // check if the file is a valid SVG
+        if ($xml->getName() !== 'svg') {
+            return '';
+        }
+
+        // set attributes 
+        foreach ($attrs as $key => $value) {
+            if (is_string($key)) {
+                $xml->addAttribute($key, $value);
+            }
+        }
+
+        $html = $xml->asXML() ?? '';
+        // remove the XML declaration
+        $html = preg_replace('/<\?xml.*?\?>/', '', $html);
+        return $html;
+    } else {
+        $icon = Icon::new($id);
+        if ($icon) {
+            return $icon->use(...$attrs);
+        }
     }
 
     return '';
